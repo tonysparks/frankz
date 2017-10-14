8dbaa52c77b4b856d609ad38e8553d0ba299c73e
tonysparks <anthony.sparks31@gmail.com>
2017-10-13 23:57:19 -0500

    WIP - deploy via command line

--- desktop/src/colony/game/desktop/DesktopLauncher.java
+++ desktop/src/colony/game/desktop/DesktopLauncher.java
@@ -1,15 +1,22 @@
 package colony.game.desktop;
 
+import java.io.File;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import colony.assets.JsonAssetLoader;
-import colony.game.ColonyGame;
+import colony.game.Game;
 import colony.game.Config;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {	    
-	    Config cfg = JsonAssetLoader.loadAsset(Config.class, "./assets/config.cfg");
+	public static void main (String[] arg) {
+	    String filename = "./config.cfg";
+	    if(new File("./assets/config.cfg").exists()) {
+	        filename = "./assets/config.cfg";
+	    }
+	    
+	    Config cfg = JsonAssetLoader.loadAsset(Config.class, filename);
 	    
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Franks";		
@@ -22,6 +29,6 @@ public class DesktopLauncher {
 		config.x = -1;
 		config.y = -1;
 				
-		new LwjglApplication(new ColonyGame(cfg), config);
+		new LwjglApplication(new Game(cfg), config);
 	}
 }
